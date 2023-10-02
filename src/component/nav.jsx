e5f94708fb66c3741426c2c952b515c604cdfbe6
ike-blessing <blessingfavour308@gmail.comclear>
2023-10-02 22:29:48 +0100

    added asset folder and design folder

---
+++ src/component/nav.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+z
+function nav() {
+
+  return (
+  <div className="w-screen h-screen flex sm:flex-col lg:flex-row justify-center items-center bg-blue-400">
+    <h1>Hello world</h1>
+  </div>
+  )
+}
+
+export default nav
