0d7ee999fc3d5434dc4831490b70ed5554d93310
ike-blessing <blessingfavour308@gmail.comclear>
2023-10-02 13:18:59 +0100

    add: app workflow

---
+++ github/workflows/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+    return (
+    <div className="w-screen h-screen flex sm:flex-col lg:flex-row justify-center items-center bg-blue-400">
+     <div className="w-40 h-40 bg-red-200"></div>
+     <div className="w-40 h-40 bg-red-400"></div>
+     <div className="w-40 h-40 bg-red-600"></div>
+     <div className="w-40 h-40 bg-red-800"></div>
+    </div>
+    )
+  }
+  
+  export default App
